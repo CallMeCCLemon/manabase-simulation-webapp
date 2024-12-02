7073a61b756aa0b5c750b4c9acccd75092f85475
Thomas Littlejohn <thomas.e.littlejohn@gmail.com>
2024-12-02 08:14:21 -0600

    Website now does... something at least to show it all works

--- src/routes/ManaSimulatorRequestForm.svelte
+++ src/routes/ManaSimulatorRequestForm.svelte
@@ -1,23 +1,16 @@
 <script lang="ts">
-	// import { onMount } from 'svelte';
-	// import { type ClientOptions, createClient } from '@urql/core';
-
-	// const options = {
-	// 	url: 'http://100.69.236.43:32001',
-	// } as ClientOptions;
-	//
-	// const graphqlClient = createClient(options);
+	
+	let message = $state("no Data yet...")
 
 	async function handleSubmit(event: Event) {
 		event.preventDefault();
-		console.log(JSON.stringify(event));
+		console.log(event.currentTarget);
+		const formData = new FormData(event.currentTarget as HTMLFormElement);
+		console.log(JSON.stringify(formData));
 
-		// const formData = new FormData(event.currentTarget as HTMLFormElement);
-		// const message = formData.get('message') as string;
-		//
 		const query = `
 		{
-    echo(message: "Hello World!") {
+    echo(message: "${formData.get('message')}") {
         message
     }
 }
@@ -34,22 +27,15 @@
 			return response.json();
 		}).then((data) => {
 			console.log(data);
+			message = data.data.echo.message;
 		});
-
-		// try {
-		// 	// const response = await graphqlClient.mutation(query, { message }).toPromise();
-		// 	console.log(response.data.echo.message);
-		// 	alert(response.data.echo.message);
-		// } catch (error) {
-		// 	console.error('Error submitting form:', error);
-		// }
 	}
 
 </script>
 
 <div>
 	This is the start of the mana request form.
-	<form>
+	<form onsubmit={handleSubmit}>
 		<div class="space-y-12">
 			<div class="border-b border-gray-900/10 pb-12">
 				<h2 class="text-2xl font-bold">Mana Simulator Echo Test Request</h2>
@@ -68,22 +54,14 @@
 								class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
 							/>
 						</div>
-					</div>
 
-<!--					<div class="sm:col-span-3">-->
-<!--						<label for="last-name" class="block text-sm/6 font-medium text-gray-900">-->
-<!--							Last name-->
-<!--						</label>-->
-<!--						<div class="mt-2">-->
-<!--							<input-->
-<!--								id="last-name"-->
-<!--								name="last-name"-->
-<!--								type="text"-->
-<!--								autoComplete="family-name"-->
-<!--								class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"-->
-<!--							/>-->
-<!--						</div>-->
+						<div class="mt-2">
+							<p class="text-sm text-gray-500">
+								Output: {message}
+							</p>
+						</div>
 					</div>
+				</div>
 
 				<div class="mt-6 flex items-center justify-end gap-x-6">
 					<button type="button" class="text-sm/6 font-semibold text-gray-900">
@@ -92,7 +70,6 @@
 					<button
 						type="submit"
 						class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-						onclick={handleSubmit}
 					>
 						Save
 					</button>
