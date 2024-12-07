0e78646b835be8a2c354127c14e681f8a933e4c9
Thomas Littlejohn <thomas.e.littlejohn@gmail.com>
2024-12-06 20:41:08 -0800

    Binding decklist to the textArea input field

--- src/routes/ManaSimulatorRequestForm.svelte
+++ src/routes/ManaSimulatorRequestForm.svelte
@@ -159,7 +159,7 @@
 					</label>
 					<div class="mt-2">
 							<textarea
-								value={deckList}
+								bind:value={deckList}
 								id="deck-list"
 								name="deck-list"
 								class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
