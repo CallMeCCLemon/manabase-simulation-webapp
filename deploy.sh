7f40d2bfc22c95514b1f2d75d3de613c9ed95f72
Thomas Littlejohn <thomas.e.littlejohn@gmail.com>
2024-12-25 18:26:24 -0700

    updating deployment script to rollout the k8s deployment as well

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 git pull
 docker build --platform=linux/amd64 -t 10.0.0.108:32000/manabase-simulation-webapp:thomas-latest -f dockerfile .
 docker push 10.0.0.108:32000/manabase-simulation-webapp:thomas-latest
+kubectl rollout restart deployment -n mtg-mana-sim-app manabase-simulation-webapp-deployment
